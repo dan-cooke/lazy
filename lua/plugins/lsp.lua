78d505a49a131fc033bc10848ea93911cab01253
Daniel Cooke <danielcooke1996@gmail.com>
2025-08-02 10:19:11 +0100

    Add lexical and vtsls config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local util = require("lspconfig.util")
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -5,9 +6,26 @@ return {
     servers = {
       eslint = {
         root_dir = function(fname)
-          local util = require("lspconfig.util")
           return util.root_pattern(".git")(fname) or util.path.dirname(fname)
         end,
+      },
+
+      vtsls = {
+        root_dir = util.root_pattern(".git"),
+        typescript = {
+          preferences = {
+            importModuleSpecifier = "relative",
+            quoteStyle = "single",
+          },
+        },
+      },
+      lexical = {
+        cmd = { "/home/dan/Sources/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+        root_dir = function(fname)
+          return util.root_pattern("mix.exs", ".git")(fname) or vim.loop.cwd()
+        end,
+
+        filetypes = { "elixir", "eelixir", "heex" },
       },
     },
   },
